56878bc4a6b127b5eb26fcdd7b8b4ded80c0218c
bharath3003 <pkbaru003@gmail.com>
2024-04-22 01:06:40 +0530

    Projects not being displayed

--- src/main/java/com/JavaProject/controller/HomeController.java
+++ src/main/java/com/JavaProject/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.JavaProject.controller;
 
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,9 +9,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.JavaProject.repository.ProjectRepository;
 import com.JavaProject.repository.UserRepo;
+import com.JavaProject.entity.Project;
+import com.JavaProject.entity.Project.DomainName;
 import com.JavaProject.entity.User;
+import com.JavaProject.service.ProjectService;
 import com.JavaProject.service.UserService;
 import com.fasterxml.jackson.annotation.JsonCreator.Mode;
 
@@ -24,6 +31,12 @@ public class HomeController {
 
     @Autowired
     private UserRepo userRepo;
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ProjectRepository projectRepository;
 
     @ModelAttribute
     public void commonUser(Principal p, Model m) {
@@ -60,12 +73,25 @@ public class HomeController {
         return "profile";
     }
 
-    @GetMapping("/user/projects")
-    public String projects() {
-        return "projects"; // Assuming "projects" is the name of the projects page HTML template
+     @GetMapping("/user/projects")
+    public String projects(Model model) {
+        // Get all projects initially
+        List<Project> allProjects = projectService.getAllProjects();
+        model.addAttribute("allProjects", allProjects);
+
+        // Return the projects.html template
+        return "projects";
     }
 
-    
+    @PostMapping("/user/projects")
+    public String projectsByDomain(@RequestParam("domain") DomainName domain, Model model) {
+        // Get projects by domain
+        List<Project> projectsByDomain = projectService.getProjectsByDomain(domain);
+        model.addAttribute("allProjects", projectsByDomain); // Update model with filtered projects
+
+        // Return the projects.html template
+        return "projects";
+    }
 
 
 
--- src/main/java/com/JavaProject/repository/ProjectRepository.java
+++ src/main/java/com/JavaProject/repository/ProjectRepository.java
@@ -1,6 +1,8 @@
 package com.JavaProject.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
 import com.JavaProject.entity.Project;
 import com.JavaProject.entity.Project.DomainName;
 
@@ -8,5 +10,6 @@ import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project, Integer> {
     // Custom query method to retrieve projects by domain
+    @Query("SELECT p FROM Project p WHERE p.domain = :domain")
     List<Project> findByDomain(DomainName domain);
 }
